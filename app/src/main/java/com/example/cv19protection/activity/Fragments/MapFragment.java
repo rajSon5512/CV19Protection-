da646d0c0b38dc562e32aeeece1f200292ca2085
rajSon5512 <rjvsonawala@gmail.com>
2020-06-22 06:06:42 -0700

    Third commit

---
+++ app/src/main/java/com/example/cv19protection/activity/Fragments/MapFragment.java
@@ -0,0 +1,25 @@
+package com.example.cv19protection.activity.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cv19protection.R;
+
+public class MapFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view=inflater.inflate(R.layout.fragment_map,container,false);
+        return view;
+    }
+}
+
+
+
--- app/src/main/java/com/example/cv19protection/activity/InnerActivity/MainActivity.java
+++ app/src/main/java/com/example/cv19protection/activity/InnerActivity/MainActivity.java
@@ -1,18 +1,69 @@
 package com.example.cv19protection.activity.InnerActivity;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.TableLayout;
 
 import com.example.cv19protection.R;
+import com.example.cv19protection.activity.Fragments.InformationFragment;
+import com.example.cv19protection.activity.Fragments.MapFragment;
+import com.example.cv19protection.activity.Fragments.SelfAssessmentFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        viewPager=findViewById(R.id.viewPager_main_screen);
+        viewPager.setAdapter(new MainViewPager(getSupportFragmentManager()));
+
+        tabLayout=findViewById(R.id.tab_layout);
+
+
+
     }
+
+
+    public class MainViewPager extends FragmentStatePagerAdapter{
+
+        public MainViewPager(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+
+
+            switch (i){
+
+                case 0:return new InformationFragment();
+                case 1:return new SelfAssessmentFragment();
+                default:return new MapFragment();
+
+            }
+
+        }
+
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+    }
+
+
 }
+
+
+
